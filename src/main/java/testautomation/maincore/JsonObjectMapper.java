76a4fb587b768405d7f93f83c3a58fda0eb8faf4
Abhishek Home <shelke.abhishek@outlook.com>
2019-08-15 22:07:28 +0530

    Commit New changes on remote

--- src/main/java/testautomation/maincore/JsonObjectMapper.java
+++ src/main/java/testautomation/maincore/JsonObjectMapper.java
@@ -2,7 +2,7 @@ package testautomation.maincore;
 
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.Method;
+
 import testautomation.jsoncontract.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.core.JsonParseException;
@@ -15,14 +15,17 @@ public class JsonObjectMapper {
 	
 	public String Mapper(String Json_source,String Json_object_name, String KeyName) throws NoSuchMethodException, SecurityException{
 		String Keyvalue = null;
-		ObjectMapper objectMapper = new ObjectMapper();
-		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-		objectMapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
+		ObjectMapper objMapper = new ObjectMapper();
+		objMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		objMapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
 		try {	
-				HLM = objectMapper.readValue(new File(Json_source) , HL_Message.class);
-				Method a = HLM.getClass().getMethod(Json_object_name);
-				a.getClass().getMethods();
-				System.out.println(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(HLM));
+				HLM = objMapper.readValue(new File(Json_source) , HL_Message.class);
+				System.out.println(HLM);
+				Keyvalue = objMapper.writerWithDefaultPrettyPrinter().writeValueAsString(HLM);
+				System.out.println(Keyvalue);
+				//Method a = HLM.getClass().getMethod(Json_object_name);
+				//a.getClass().getMethods();
+				
 			}
 		catch (JsonMappingException e) {e.printStackTrace();}
 		catch (JsonParseException  e) {e.printStackTrace();}
--- src/test/java/testautomation/cases/Json_check.java
+++ src/test/java/testautomation/cases/Json_check.java
@@ -2,29 +2,17 @@ package testautomation.cases;
 
 import org.testng.annotations.Test;
 import testautomation.maincore.*;
-import org.testng.annotations.BeforeTest;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterTest;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Json_check {
-	WebDriver driver = new FirefoxDriver();	
+	//WebDriver driver = new FirefoxDriver();	
   @Test
   public void Test_Json() throws NoSuchMethodException, SecurityException {
 	  //System.setProperty("webdriver.gecko.driver","F:\\iBridge\\Abhishek\\Automation\\Selenium Automation\\Framework Samples\\JavaJsonParser\\geckodriver.exe");
 	  //driver.get("https://live.ibridgeworkflow.com/iBridge/Page/Page.html");
 	  JsonObjectMapper JOM = new JsonObjectMapper();
-	  JOM.Mapper(System.getProperty("user.dir")+"\\src\\main\\java\\testautomation\\maincore\\Sample.json", "proceduredetails", "ExternalAccessionNumber");
+	  JOM.Mapper(System.getProperty("user.dir")+"\\src\\main\\java\\testautomation\\maincore\\Sample.json", "appointment", "Facility");
 	  
   }
-  @BeforeTest
-  public void beforeTest() {
-	  
-  }
-
-  @AfterTest
-  public void afterTest() {
-	  driver.quit();
-  }
-
 }
